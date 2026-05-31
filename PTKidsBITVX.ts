210f4cb24cf5baf809aee1aad93319f718155bcc
pt-robotics <ptroboticsth@gmail.com>
2026-05-31 11:13:38 +0700

    Update PTKidsBITVX.ts

--- PTKidsBITVX.ts
+++ PTKidsBITVX.ts
@@ -214,40 +214,44 @@ namespace PTKidsBITVX {
         if (initI2C == false) {
             initPCA()
         }
-
+        let i2cData = pins.createBuffer(2)
+        let start = 0
         let angle_input = pins.map(angle, 0, 180, -90, 90)
         angle = Math.max(Math.min(90, angle_input), -90)
-
         let stop = 369 + angle * 235 / 90
-        let reg = SERVOS + servo * 4
-        let buf = pins.createBuffer(5)
+        i2cData[0] = SERVOS + servo * 4 + 2
+        i2cData[1] = (stop & 0xff)
+        pins.i2cWriteBuffer(PCA, i2cData, false)
 
-        buf[0] = reg
-        buf[1] = 0x00
-        buf[2] = 0x00
-        buf[3] = stop & 0xff
-        buf[4] = (stop >> 8) & 0x0f
-
-        pins.i2cWriteBuffer(PCA, buf, false)
+        i2cData[0] = SERVOS + servo * 4 + 3
+        i2cData[1] = (stop >> 8)
+        pins.i2cWriteBuffer(PCA, i2cData, false)
     }
 
     function analogWritePCA(channel: number, value: number): void {
         if (initI2C == false) {
-            initPCA()
+            initPCA();
         }
 
-        value = Math.max(0, Math.min(4095, value))
+        value = Math.max(0, Math.min(4095, value));
+        let onValue = 0;
+        let offValue = value;
+        let i2cData = pins.createBuffer(2);
+        i2cData[0] = SERVOS + channel * 4;
+        i2cData[1] = onValue & 0xff;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
 
-        let reg = SERVOS + channel * 4
-        let buf = pins.createBuffer(5)
+        i2cData[0] = SERVOS + channel * 4 + 1;
+        i2cData[1] = (onValue >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
 
-        buf[0] = reg
-        buf[1] = 0x00
-        buf[2] = 0x00
-        buf[3] = value & 0xff
-        buf[4] = (value >> 8) & 0x0f
+        i2cData[0] = SERVOS + channel * 4 + 2;
+        i2cData[1] = offValue & 0xff;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
 
-        pins.i2cWriteBuffer(PCA, buf, false)
+        i2cData[0] = SERVOS + channel * 4 + 3;
+        i2cData[1] = (offValue >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA, i2cData, false);
     }
 
     function initBNO055() {
@@ -807,7 +811,7 @@ namespace PTKidsBITVX {
             pins.digitalWritePin(DigitalPin.P2, 0)
             duration = pins.pulseIn(DigitalPin.P1, PulseValue.High, maxCmDistance * 58)
         }
-        
+
         let d = Math.idiv(duration, 58)
 
         if (d != 0) {
